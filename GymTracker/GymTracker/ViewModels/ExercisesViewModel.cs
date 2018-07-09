4336e872af70983f752c7ad5cef56d5e378581db
mgce <blogcontend@gmail.com>
2018-07-09 18:54:03 +0200

    Fixed loading exercises list on exercises page

--- GymTracker/GymTracker/ViewModels/ExercisesViewModel.cs
+++ GymTracker/GymTracker/ViewModels/ExercisesViewModel.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
+using System.Threading.Tasks;
 using GymTracker.Helpers;
 using GymTracker.Models;
+using GymTracker.Repositories;
+using GymTracker.Views;
 using Prism.Commands;
 using Prism.Navigation;
 
@@ -11,10 +14,15 @@ namespace GymTracker.ViewModels
 {
     public class ExercisesViewModel : ViewModelBase
     {
-        public ExercisesViewModel(INavigationService navigationService) 
+        private readonly IExerciseRepository _exerciseRepository;
+        public DelegateCommand AddNewExerciseCommand { get; }
+
+        public ExercisesViewModel(INavigationService navigationService, IExerciseRepository exerciseRepository) 
             : base(navigationService)
         {
+            _exerciseRepository = exerciseRepository;
             Exercises = new ObservableCollection<Exercise>();
+            AddNewExerciseCommand = new DelegateCommand(async ()=>await  AddNewExercise());
         }
 
         private ObservableCollection<Exercise> _exercises;
@@ -31,7 +39,20 @@ namespace GymTracker.ViewModels
             set => SetProperty(ref _stages, value);
         }
 
-        public override void OnNavigatedTo(NavigationParameters parameters)
+        private async Task LoadExercises()
+        {
+            var exercises = await _exerciseRepository.GetByStageId(Stage.Id);
+            Exercises.AddRange(exercises);
+        }
+
+        private async Task AddNewExercise()
+        {
+            var navigationParams = new NavigationParameters();
+            navigationParams.Add(Constants.Models.Stage, Stage);
+            await NavigationService.NavigateAsync(nameof(AddExercisePage), navigationParams);
+        }
+
+        public override async void OnNavigatedTo(NavigationParameters parameters)
         {
             if (parameters.ContainsKey(Constants.Models.Stage))
             {
@@ -44,6 +65,8 @@ namespace GymTracker.ViewModels
                 var exercise = parameters.GetValue<Exercise>(Constants.Models.NewExercise);
                 Exercises.Add(exercise);
             }
+
+            await LoadExercises();
         }
     }
 }
